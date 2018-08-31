c6eff607313291996e2110c2568b21556bffcdad
yari <yamudriy@gmail.com>
2018-08-31 14:59:33 +0300

    Change AWS provider version to 1.34.0 for Module 06

--- 06/demos/networking/resources.tf
+++ 06/demos/networking/resources.tf
@@ -6,7 +6,7 @@ provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "eu-central-1"
-  version    = "~> 1.33.0"
+  version    = "~> 1.34.0"
 }
 
 ##################################################################################
