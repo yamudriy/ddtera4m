9bbcfb199f68ab76dc2464aa455dc4855bf493f6
yari <yamudriy@gmail.com>
2018-08-31 14:47:28 +0300

    Use fresh AWS provider version for Module 06

--- 06/demos/networking/resources.tf
+++ 06/demos/networking/resources.tf
@@ -6,6 +6,7 @@ provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "eu-central-1"
+  version    = "~> 1.33.0"
 }
 
 ##################################################################################
