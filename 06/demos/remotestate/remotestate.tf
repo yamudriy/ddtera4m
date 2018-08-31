733f381b9a7cc25888d10adafbcafc39e29c6e19
yari <yamudriy@gmail.com>
2018-08-31 13:45:35 +0300

    Adjustments to run Module 06 Demo from GitHub

--- 06/demos/remotestate/remotestate.tf
+++ 06/demos/remotestate/remotestate.tf
@@ -6,15 +6,15 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 variable "aws_networking_bucket" {
-  default = "ddt-networking"
+  default = "dddtt-networking"
 }
 
 variable "aws_application_bucket" {
-  default = "ddt-application"
+  default = "dddtt-application"
 }
 
 variable "aws_dynamodb_table" {
-  default = "ddt-tfstatelock"
+  default = "dddtt-tfstatelock"
 }
 
 ##################################################################################
@@ -24,7 +24,7 @@ variable "aws_dynamodb_table" {
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region     = "us-west-2"
+  region     = "eu-central-1"
 }
 
 ##################################################################################
